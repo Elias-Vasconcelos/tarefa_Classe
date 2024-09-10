f17b8443949c03310ca2b7b63a47fe2a0faf743e
Elias-Vasconcelos <146135801+Elias-Vasconcelos@users.noreply.github.com>
2024-09-10 20:30:21 -0300

    Corraçao dos comentarios

--- Tarefa_calsse/src/Carro.java
+++ Tarefa_calsse/src/Carro.java
@@ -1,115 +1,111 @@
 /**
  * @author Elias-Fereira
  */
-
 public class Carro {
     private Boolean estaLigado = false;
     private int velocidadeAtual = 0;
     private int velocidadeMaxima = 120;
     private int combustivel = 40;
-    public  String mensagemDeErro = "Por faovr Lige o carro";
+    public String mensagemDeErro = "Por favor, ligue o carro";
 
-public static void main(String[] args) {
-    Carro carroDeCorrida = new Carro();
+    public static void main(String[] args) {
+        Carro carroDeCorrida = new Carro();
 
-    carroDeCorrida.ligarOuDesligarCarro();
-    carroDeCorrida.acelerar();
-    carroDeCorrida.acelerar();
-    carroDeCorrida.acelerar();
-    carroDeCorrida.ligarOuDesligarCarro();
-    carroDeCorrida.reabastecer();
-    carroDeCorrida.ligarOuDesligarCarro();
-    carroDeCorrida.acelerar();
-    carroDeCorrida.acelerar();
-    carroDeCorrida.desacelerar();
+        carroDeCorrida.ligarOuDesligarCarro();
+        carroDeCorrida.acelerar();
+        carroDeCorrida.acelerar();
+        carroDeCorrida.acelerar();
+        carroDeCorrida.ligarOuDesligarCarro();
+        carroDeCorrida.reabastecer();
+        carroDeCorrida.ligarOuDesligarCarro();
+        carroDeCorrida.acelerar();
+        carroDeCorrida.acelerar();
+        carroDeCorrida.desacelerar();
 
-    carroDeCorrida.getStats();
-
+        carroDeCorrida.getStats();
     }
 
     /**
-     *@param(null) Este metodo Não recebe parametros
+     * Liga ou desliga o carro.
      *
-     *@return Este metodo altera o valor da varaioavel estaLigado, e a varaialvel velocidadeAtual para 0
+     * @return Este método altera o valor da variável estaLigado e reseta a velocidadeAtual para 0.
      */
     public void ligarOuDesligarCarro() {
-       this.estaLigado = !this.estaLigado;
-       this.combustivel -= 5;
-       velocidadeAtual = 0;
+        this.estaLigado = !this.estaLigado;
+        this.combustivel -= 5;
+        velocidadeAtual = 0;
     }
 
-
     /**
-     *@param(null) Este metodo Não recebe parametros
+     * Acelera o carro.
      *
-     *@throws this.mensagemDeErro caso o carro esteja desligado,sem combustivel ou esteja na velocidade maxiam.
-     *
-     *@return  Este metodo altera o valor da varaioavel velocidadeAtual ate o maximo de 120
+     * @throws IllegalStateException caso o carro esteja desligado, sem combustível ou na velocidade máxima.
+     * @return Este método aumenta a velocidadeAtual até o máximo de 120.
      */
     public void acelerar() {
-        if (this.estaLigado){
-            if(this.combustivel == 0 ) {
-                System.out.print("O Carro ficou sem combustivel! por favor REABASTEÇA");
-            }else
-                if(this.velocidadeAtual == this.velocidadeMaxima) {
-                System.out.print("O carro ja esta na velocidade Maxima!");
-                    }else
-                        this.velocidadeAtual += 10;
-                        this.combustivel -= 5;
-            } else System.out.println(mensagemDeErro);
+        if (this.estaLigado) {
+            if (this.combustivel == 0) {
+                System.out.println("O carro ficou sem combustível! Por favor, reabasteça.");
+            } else if (this.velocidadeAtual == this.velocidadeMaxima) {
+                System.out.println("O carro já está na velocidade máxima!");
+            } else {
+                this.velocidadeAtual += 10;
+                this.combustivel -= 5;
+            }
+        } else {
+            throw new IllegalStateException(mensagemDeErro);
         }
-
+    }
 
     /**
-     *@param(null) Este metodo Não recebe parametros
+     * Desacelera o carro.
      *
-     *@throws this.mensagemDeErro caso o carro esteja desligado, ou se a velocidadeAtual for igual a 0.
-     *
-     *@return  Este metodo decrementa o valor da varaioavel velocidadeAtual ate que ele chegue a 0
+     * @throws IllegalStateException caso o carro esteja desligado ou a velocidadeAtual seja igual a 0.
+     * @return Este método reduz a velocidadeAtual até 0.
      */
-    public void desacelerar () {
-        if (this.estaLigado){
-            if(this.combustivel == 0 ) {
-                System.out.print("O Carro ficou sem combustivel! por favor REABASTEÇA");
-            }else
-            if(this.velocidadeAtual == 0) {
+    public void desacelerar() {
+        if (this.estaLigado) {
+            if (this.combustivel == 0) {
+                System.out.println("O carro ficou sem combustível! Por favor, reabasteça.");
+            } else if (this.velocidadeAtual == 0) {
                 this.estaLigado = false;
-                System.out.print("O carrto parou! Por favor ligue o carro novamente!");
-            }else
+                System.out.println("O carro parou! Por favor, ligue o carro novamente.");
+            } else {
                 this.velocidadeAtual -= 10;
-            this.combustivel -= 5;
-        } else System.out.println(mensagemDeErro);
+                this.combustivel -= 5;
+            }
+        } else {
+            throw new IllegalStateException(mensagemDeErro);
+        }
     }
 
-
     /**
-     *@param(null) Este metodo Não recebe parametros
+     * Reabastece o carro.
      *
-     *@throws this.mensagemDeErro caso o carro esteja Ligado.
-     *
-     *@return  Este metodo altera o valor da varaioavel combustivel para 40
+     * @throws IllegalStateException caso o carro esteja ligado.
+     * @return Este método preenche o combustível até 40.
      */
-    public void reabastecer () {
-        if (estaLigado){
-            System.out.println("Por favor Desligue o carro antes de reabastecer!");
-        } else
-            if(this.combustivel == 40){
-            System.out.print("O tanque de combustivel esta cheio!");
-        } else this.combustivel = 40;
+    public void reabastecer() {
+        if (estaLigado) {
+            throw new IllegalStateException("Por favor, desligue o carro antes de reabastecer!");
+        } else if (this.combustivel == 40) {
+            System.out.println("O tanque de combustível está cheio!");
+        } else {
+            this.combustivel = 40;
+        }
     }
 
-
     /**
-     *@param(null) Este metodo Não recebe parametros
+     * Exibe o status atual do carro.
      *
-     *@return  Este metodo Imprime o valor atual das variaveis do carro instanciado no console
+     * @return Este método imprime o valor atual das variáveis do carro no console.
      */
-    public void getStats () {
+    public void getStats() {
         System.out.println("-------------------------------------");
         System.out.println("O status atual do carro é:");
-        System.out.println("Esata:" + " " + (this.estaLigado? "Ligado" : "Desligado") );
-        System.out.println("Velocidade:" + " " + this.velocidadeAtual);
-        System.out.println("Combustivel:" + " " + this.combustivel );
+        System.out.println("Está: " + (this.estaLigado ? "Ligado" : "Desligado"));
+        System.out.println("Velocidade: " + this.velocidadeAtual);
+        System.out.println("Combustível: " + this.combustivel);
         System.out.println("-------------------------------------");
     }
 }
